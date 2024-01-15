a59b06110eb42d785a545e8c2096b31ce8d3e9d3
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-15 17:08:25 +0300

    first commit in airbnb v2

--- console.py
+++ console.py
@@ -72,15 +72,25 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, args):
-        """Creats a new instance of the BaseModel"""
-        arg = parse(args)
-        if len(arg) == 0:
+        """ Create an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                arg_splited = arg.split("=")
+                arg_splited[1] = eval(arg_splited[1])
+                if type(arg_splited[1]) is str:
+                    arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
+                kw[arg_splited[0]] = arg_splited[1]
+        except SyntaxError:
             print("** class name missing **")
-        elif arg[0] not in HBNBCommand.__commands:
+        except NameError:
             print("** class doesn't exist **")
-        else:
-            print(eval(arg[0])().id)
-            storage.save()
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance.save()
+        print(new_instance.id)
 
     def do_show(self, args):
         """ shows the instance when invoked """
