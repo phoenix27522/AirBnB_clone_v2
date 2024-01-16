bc8beb879235de58d75457660a49fc00dc063fcc
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-16 19:53:01 +0300

    fix a bug on task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
-
+"""This is the file storage class for AirBnB"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -13,55 +12,69 @@ import shlex
 
 
 class FileStorage:
-    """class that stores info and help to serialize and desrialize.
-
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
     Attributes:
-        __file_path(str): the name of the file to the objects to.
-        __objects(dict): A dictionary of instantiated objects.
+        __file_path: path to the JSON file
+        __objects: objects will be stored
     """
-
     __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls =None):
-        """Returns the dictionary of __objects."""
+    def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
         dic = {}
         if cls:
             dictionary = self.__objects
             for key in dictionary:
-                part = key.replace('.', ' ')
-                part = shlex.split(part)
-                if (part[0] == cls.__name__):
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
                     dic[key] = self.__objects[key]
-                return (dic)
+            return (dic)
         else:
             return self.__objects
 
     def new(self, obj):
-        """Sets a unique identifier "id" """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Serialize the __objects to a JSON file."""
-        with open(FileStorage.__file_path, "w") as file:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
-
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Deserialize the json file """
+        """serialize the file path to JSON file path
+        """
         try:
-            with open(FileStorage.__file_path) as file:
-                new_dict = json.load(file)
-                for i in new_dict.values():
-                    name_cls = i["__class__"]
-                    del i["__class__"]
-                    self.new(eval(name_cls)(**i))
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
-            return
+            pass
 
     def delete(self, obj=None):
-        """ deleting existing element"""
+        """ delete an existing element
+        """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """ calls reload()
+        """
+        self.reload()
