6095c3a03a7ad0a674501463b8d656b0e0a31a25
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-31 22:21:39 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run, sudo
 
 env.hosts = ["3.80.18.6", "3.84.238.206"]
 
@@ -18,32 +16,56 @@ def do_deploy(archive_path):
         Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
+        print("Error: Archive file does not exist.")
         return False
+
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
+    print("Running command: put({}, /tmp/{})".format(archive_path, file))
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        print("Error: Failed to upload the archive.")
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    print("Running command: sudo rm -rf /data/web_static/releases/{}/".format(name))
+    if sudo("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+        print("Error: Failed to remove existing release directory.")
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    print("Running command: sudo mkdir -p /data/web_static/releases/{}/".format(name))
+    if sudo("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+        print("Error: Failed to create release directory.")
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+
+    print("Running command: sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+    if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
+        print("Error: Failed to extract archive.")
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+
+    print("Running command: sudo rm /tmp/{}".format(file))
+    if sudo("rm /tmp/{}".format(file)).failed is True:
+        print("Error: Failed to remove temporary archive.")
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+
+    print("Running command: sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+    if sudo("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed is True:
+        print("Error: Failed to move files within the release directory.")
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+
+    print("Running command: sudo rm -rf /data/web_static/releases/{}/web_static".format(name))
+    if sudo("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
+        print("Error: Failed to remove redundant web_static directory.")
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+
+    print("Running command: sudo rm -rf /data/web_static/current")
+    if sudo("rm -rf /data/web_static/current").failed is True:
+        print("Error: Failed to remove current symbolic link.")
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+
+    print("Running command: sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+    if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
+        print("Error: Failed to create new symbolic link.")
         return False
+
+    print("Deployment successful.")
     return True
