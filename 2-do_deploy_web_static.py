3478a89af60abd7783ea4812c017154e1516140b
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-01 00:16:15 +0300

    fix 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to web servers
-"""
-
-from fabric.api import local, env, put, run
-from os import path
-
+"""script that distributes an archive to your web servers"""
+from fabric.api import *
+from datetime import datetime
+import os
 env.hosts = ["3.80.18.6", "3.84.238.206"]
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a .tgz archive from the contents"""
+    try:
+        local('mkdir -p versions')
+        date_format = '%Y%m%d%H%M%S'
+        arch_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(date_format))
+        local('tar -cvzf {} web_static'.format(arch_path))
+        print('web_static packed: {} -> {}'.format(arch_path,
+              os.path.getsize(arch_path)))
+    except TypeError:
+        return None
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    """
-    if not path.exists(archive_path):
+    """distributes an archive to your web servers"""
+    if not os.path.exists(archive_path):
         return False
-
+    f_name = archive_path.split('/')[1]
+    f_path = '/data/web_static/releases/'
+    r_path = f_path + f_name[:-4]
     try:
-        # Upload the archive to /tmp/ directory on the web server
-        put(archive_path, "/tmp/")
-
-        # Extract the archive to /data/web_static/releases/<archive_filename without extension>
-        archive_filename = archive_path.split("/")[-1]
-        archive_folder = "/data/web_static/releases/{}".format(
-            archive_filename.split(".")[0])
-        run("mkdir -p {}".format(archive_folder))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
-
-        # Remove the uploaded archive from the web server
-        run("rm /tmp/{}".format(archive_filename))
-
-        # Create a symbolic link /data/web_static/current pointing to the new version
-        run("rm -f /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(archive_folder))
-
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(r_path))
+        run('tar -xzf /tmp/{} -C {}'.format(f_name, r_path))
+        run('rm /tmp/{}'.format(f_name))
+        run('mv {}/web_static/* {}/'.format(r_path, r_path))
+        run('rm -rf {}/web_static'.format(r_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(r_path))
+        print('New version deployed!')
         return True
-
-    except Exception as e:
-        print("Deployment failed:", str(e))
+    except TypeError:
         return False
