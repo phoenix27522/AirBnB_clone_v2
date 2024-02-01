e57fc7dc94e7241491b6dee01555a4a46f4d95c2
nahom <kiflunahom1994@gmail.com>
2024-02-01 11:57:25 -0800

    fix task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,7 +10,6 @@ from fabric.api import *
 
 env.hosts = ["3.80.18.6", "3.84.238.206"]
 
-
 def do_clean(number=0):
     """Delete out-of-date archives.
     Args:
@@ -19,15 +18,18 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0 else int(number)
+    number = max(int(number), 1)  # Ensure number is at least 1
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    try:
+        with lcd("versions"):
+            archives_local = sorted(os.listdir("."))
+            [local("rm ./{}".format(a)) for a in archives_local[:-number]]
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        with cd("/data/web_static/releases"):
+            archives_remote = sorted(run("ls -tr").split())
+            archives_remote = [a for a in archives_remote if "web_static_" in a]
+            [run("rm -rf ./{}".format(a)) for a in archives_remote[:-number]]
+
+    except Exception as e:
+        print(f"Error: {e}")
+
