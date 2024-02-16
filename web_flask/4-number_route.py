b4e0bd2a0c921ddfda228153dfe11d48b591a089
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-16 19:38:05 +0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,7 +28,7 @@ def hbnb():
 @app.route("/c/<text>",  strict_slashes=False)
 def c(text):
     """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_"," ")
+    text = text.replace("_", " ")
     return "C {}".format(text)
 
 
